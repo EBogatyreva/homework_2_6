8b2408d4b8b3d291337520fea93322d0c8f9e7dd
L <EBogatyrevaPK@yandex.ru>
2022-04-02 12:39:50 +1200

    Done!

--- src/pro/sky/java/course2/Main.java
+++ src/pro/sky/java/course2/Main.java
@@ -1,7 +1,24 @@
 package pro.sky.java.course2;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        //Задание 1,2
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        List<Integer> nums1 = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+
+        MyListService.printOddNumbers(nums);
+        MyListService.printOvenNumbers(nums1);
+
+        //задание 3,4
+        List<String> poem = new ArrayList<>(Arrays.asList(
+                "cherry", "blossom",
+                "different", "copy",
+                "blossom", "cherry", "copy"));
+
+        MyListService.findUnique(poem);
+        MyListService.findNotUnique(poem);
     }
 }
