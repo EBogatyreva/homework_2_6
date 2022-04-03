7c01acca00b5fe0c504e52d8059be1052b0d92fd
L <EBogatyrevaPK@yandex.ru>
2022-04-03 13:32:21 +1200

    Done!

--- src/pro/sky/java/course2/MyListService.java
+++ src/pro/sky/java/course2/MyListService.java
@@ -7,27 +7,27 @@ import java.util.Set;
 
 public class MyListService {
 
-    public static void printOddNumbers(List list) {
-        for (int i = 0; i < list.size(); i++) {
-            Integer a = (Integer) list.get(i);
+    public static void printOddNumbers(List<Integer> list) {
+        for (Integer integer : list) {
+            Integer a = integer;
             if (a % 2 == 0) {
-                System.out.print(list.get(i) + " ");
+                System.out.print(integer + " ");
             }
         }
         System.out.println(" ");
     }
 
-    public static void printOvenNumbers(List list) {
-        for (int i = 0; i < list.size(); i++) {
-            Integer a = (Integer) list.get(i);
+    public static void printOvenNumbers(List<Integer> list) {
+        for (Integer integer : list) {
+            Integer a = integer;
             if (a % 2 != 0) {
-                System.out.print(list.get(i) + " ");
+                System.out.print(integer + " ");
             }
         }
         System.out.println(" ");
     }
 
-    public static void findNotUnique(List list) {
+    public static void findNotUnique(List<String> list) {
         int count = 0;
         Set<String> uniqueStrings = new HashSet<>(list);
 
@@ -37,12 +37,12 @@ public class MyListService {
         } else System.out.println("Все значения уникальны");
     }
 
-    public static void findUnique(List list) {
+    public static void findUnique(List<String> list) {
         Set<String> uniqueStrings1 = new HashSet<>();
 
         for (int i = 0; i < list.size(); i++) {
             if (Collections.frequency(list, list.get(i)) > 1) {
-                uniqueStrings1.add((String) list.get(i));
+                uniqueStrings1.add(list.get(i));
             }
         }
         System.out.println(uniqueStrings1);
